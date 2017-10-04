2a7ed47ea6ea15bb1c0b814e93c61511bd496b27
Jack Seabolt <jackseabolt@gmail.com>
2017-10-04 10:59:52 -0700

    key added to messages

--- src/components/messageList.js
+++ src/components/messageList.js
@@ -5,7 +5,7 @@ import './messageList.css';
 export default function MessageList(props) {
 
   const mappedArr = props.msgArr.map( (msg, idx) => (
-    <li>{msg}</li>
+    <li key={idx}>{msg}</li>
   ));
 
 
